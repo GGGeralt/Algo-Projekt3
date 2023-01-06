5532a6fb6a159e76731f5092893926fa03460dd9
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-01-06 20:38:56 +0100

    projekt

---
+++ Projekt3/Program.cs
@@ -0,0 +1,159 @@
+﻿using Projekt3;
+using System.ComponentModel.Design;
+using System.Drawing;
+
+/*
+ * Zbadać doświadczalnie średnią wysokość drzew BST (z duplikatami) budowanych ze słów języka polskiego.  
+Doświadczenia przeprowadzić dla drzew o różnej ilości elementów (100 do 10 000 z krokiem 100) times losowych słów ze zbiorów: 
+
+- {ALA, OLA, KOS, PIES}; 
+- słów o długości do 4 liter; 
+- wszystkich słów. 
+
+Sprawdzić średnią ilość operacji
+
+porównania dla tych
+
+przypadków przy: 
+
+- wstawianiu,  
+- wyszukiwaniu, 
+- usuwaniu elementów z drzewa.  
+
+Porównać odpowiednie przypadki times podać ograniczenia funkcyjne dla poszczególnych z nich. 
+*/
+
+public enum WordsType
+{
+    StandardWords,
+    LessEqualFourWords,
+    AllWords
+}
+
+class Program
+{
+    static string krzyzacy = "krzyzacySlowa.txt";
+
+    static string standardWords = "standardWords.txt";
+    static string lessEqual4Words = "mniejRowne4Slowa.txt";
+    static string allWords = "wszystkieSlowa.txt";
+
+    static int thingsCount = 5;
+
+    static void Main()
+    {
+        Run(WordsType.StandardWords);
+        Run(WordsType.LessEqualFourWords);
+        Run(WordsType.AllWords);
+    }
+
+    static void Run(WordsType type)
+    {
+        Random rand = new Random();
+        string[] words = GetWords(type);
+
+        int[] averageDepths = new int[100];
+        int[] averageInsertMoves = new int[100];
+        int[] averageSearchMoves = new int[100];
+        int[] averagetDeleteMoves = new int[100];
+
+        //for every size
+        for (int size = 100; size <= 10 * 1000; size += 100)
+        {
+            int sizeIndex = size / 100 - 1;
+
+            int totalDepth = 0;
+            int totalInsert = 0;
+            int totalSearch = 0;
+            int totalDelete = 0;
+            //x times for better rounding
+            for (int times = 0; times < thingsCount; times++)
+            {
+                Tree tree = new Tree();
+
+                //filling the tree
+                for (int i = 0; i < size; i++)
+                {
+                    tree.Insert(words[GetRandomIndex(words.Length)], false);
+                }
+                //get depth of each tree
+                totalDepth += tree.depth;
+                //x times again, for better rounded data
+                for (int j = 0; j < thingsCount * 2; j++)
+                {
+                    tree.Insert(words[GetRandomIndex(words.Length)], true);
+                    tree.Search(words[GetRandomIndex(words.Length)], true);
+                    tree.Delete(words[GetRandomIndex(words.Length)], true);
+                }
+                //save sum of things of doing it x times per size
+                totalInsert += Tree.InsertMoveCount;
+                totalSearch += Tree.SearchMoveCount;
+                totalDelete += Tree.DeleteMoveCount;
+
+            }
+
+            Console.WriteLine($"SIZE = {size} Index---({sizeIndex})");
+            averageDepths[sizeIndex] = totalDepth / (thingsCount);
+            averageInsertMoves[sizeIndex] = totalInsert / (thingsCount * 2);
+            averageSearchMoves[sizeIndex] = totalSearch / (thingsCount * 2);
+            averagetDeleteMoves[sizeIndex] = totalDelete / (thingsCount * 2);
+            //Console.WriteLine($"average depth={totalDepth / thingsCount}");
+            //Console.WriteLine($"average InsertMoveCount={totalInsert / thingsCount}");
+            //Console.WriteLine($"average SearchMoveCount={totalSearch / thingsCount}");
+            //Console.WriteLine($"average DeleteMoveCount={totalDelete / thingsCount}");
+
+            totalDepth = 0;
+            Tree.ResetData();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"average depth:");
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"{averageDepths[i]} ");
+        }
+
+        Console.WriteLine($"average Insert Moves:");
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"{averageInsertMoves[i]} ");
+        }
+
+        Console.WriteLine($"average Search Moves:");
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"{averageSearchMoves[i]} ");
+        }
+
+        Console.WriteLine($"averaget Delete Moves:");
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"{averagetDeleteMoves[i]} ");
+        }
+    }
+
+    static int GetRandomIndex(int size)
+    {
+        Random rand = new Random();
+        return rand.Next() % size;
+    }
+    static string[] GetWords(WordsType type)
+    {
+        string[] words = new string[File.ReadAllText(krzyzacy).Length];
+        switch (type)
+        {
+            case WordsType.StandardWords:
+                words = File.ReadAllLines(standardWords);
+                break;
+            case WordsType.LessEqualFourWords:
+                words = File.ReadAllLines(lessEqual4Words);
+                break;
+            case WordsType.AllWords:
+                words = File.ReadAllLines(allWords);
+                break;
+            default:
+                break;
+        }
+        return words;
+    }
+}
---
+++ Projekt3/Tree.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Projekt3
+{
+    public class Tree
+    {
+        public static int InsertMoveCount = 0;
+        public static int SearchMoveCount = 0;
+        public static int DeleteMoveCount = 0;
+
+        public Node root;
+        public int depth
+        {
+            get
+            {
+                return GetDepth(root);
+            }
+            set
+            {
+                depth = value;
+            }
+        }
+        public Tree()
+        {
+            root = null;
+        }
+        public Tree(string value)
+        {
+            root = new Node(value);
+        }
+
+        #region insert
+        public void Insert(string value, bool shouldCount)
+        {
+            root = InsertRec(root, value, shouldCount);
+        }
+
+        public Node InsertRec(Node actualRoot, string value, bool shouldCount)
+        {
+            if (shouldCount == false)
+            {
+                InsertMoveCount = 0;
+            }
+
+            if (actualRoot == null)
+            {
+                if (shouldCount)
+                {
+                    InsertMoveCount++;
+                }
+
+                actualRoot = new Node(value);
+                return actualRoot;
+            }
+
+            if (string.Compare(actualRoot.value, value) > 0)
+            {
+                if (shouldCount)
+                {
+                    InsertMoveCount++;
+                }
+
+                actualRoot.left = InsertRec(actualRoot.left, value, shouldCount);
+            }
+            else if (string.Compare(actualRoot.value, value) <= 0)
+            {
+                if (shouldCount)
+                {
+                    InsertMoveCount++;
+                }
+
+                actualRoot.right = InsertRec(actualRoot.right, value, shouldCount);
+            }
+
+            return actualRoot;
+        }
+        #endregion insert
+
+        #region search
+        public Node Search(string value, bool shouldCount)
+        {
+            return SearchRec(root, value, shouldCount);
+        }
+
+        public Node SearchRec(Node actualRoot, string value, bool shouldCount)
+        {
+            if (actualRoot == null || actualRoot.value == value)
+            {
+                if (shouldCount)
+                {
+                    SearchMoveCount++;
+                }
+
+                return actualRoot;
+            }
+            if (string.Compare(actualRoot.value, value) < 0)
+            {
+                if (shouldCount)
+                {
+                    SearchMoveCount++;
+                }
+
+                return SearchRec(actualRoot.right, value, shouldCount);
+            }
+            else
+            {
+                if (shouldCount)
+                {
+                    SearchMoveCount++;
+                }
+
+                return SearchRec(actualRoot.left, value, shouldCount);
+            }
+        }
+        #endregion search
+
+        #region delete
+        public void Delete(string value, bool shouldCount)
+        {
+            root = DeleteRec(root, value, shouldCount);
+        }
+
+        public Node DeleteRec(Node actualRoot, string value, bool shouldCount)
+        {
+            if (actualRoot == null)
+            {
+                return actualRoot;
+            }
+
+            if (string.Compare(actualRoot.value, value) > 0)
+            {
+                if (shouldCount)
+                {
+                    DeleteMoveCount++;
+                }
+                actualRoot.left = DeleteRec(actualRoot.left, value, shouldCount);
+            }
+            else if (string.Compare(actualRoot.value, value) < 0)
+            {
+                if (shouldCount)
+                {
+                    DeleteMoveCount++;
+                }
+                actualRoot.right = DeleteRec(actualRoot.right, value, shouldCount);
+            }
+            else
+            {
+                if (actualRoot.left == null)
+                {
+                    return actualRoot.right;
+                }
+                else if (actualRoot.right == null)
+                {
+                    return actualRoot.left;
+                }
+                else
+                {
+                    actualRoot.value = minValue(actualRoot.right);
+
+                    if (shouldCount)
+                    {
+                        DeleteMoveCount++;
+                    }
+                    actualRoot.right = DeleteRec(actualRoot.right, actualRoot.value, shouldCount);
+                }
+            }
+            return actualRoot;
+        }
+
+        string minValue(Node actualRoot)
+        {
+            string minv = actualRoot.value;
+            while (actualRoot.left != null)
+            {
+                minv = actualRoot.left.value;
+                actualRoot = actualRoot.left;
+            }
+            return minv;
+        }
+
+
+        #endregion delete
+        #region writeOut
+        public void Print()
+        {
+            inorder();
+            Console.WriteLine();
+        }
+
+        void inorder()
+        {
+            inorderRec(root);
+        }
+
+        void inorderRec(Node root)
+        {
+            if (root != null)
+            {
+                inorderRec(root.left);
+                Console.Write(root.value + " ");
+                inorderRec(root.right);
+            }
+        }
+
+        #endregion writeOut
+
+        int GetDepth(Node actualRoot)
+        {
+            if (actualRoot == null)
+            {
+                return 0;
+            }
+
+            int leftDepth = GetDepth(actualRoot.left);
+            int rightDepth = GetDepth(actualRoot.right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+
+        public static void ResetData()
+        {
+            InsertMoveCount = 0;
+            SearchMoveCount = 0;
+            DeleteMoveCount = 0;
+        }
+    }
+
+    public class Node
+    {
+        public string value;
+        public Node left = null;
+        public Node right = null;
+
+        public Node()
+        {
+            value = "";
+        }
+
+        public Node(string value)
+        {
+            this.value = value;
+        }
+    }
+}
+
